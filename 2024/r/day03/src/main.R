6dd4b3dd24e80c3f7a6240a3f8e08a43d9b2e5ae
FactOREO <hdustin@gmx.de>
2024-12-03 07:23:52 +0100

    day03

---
+++ 2024/r/day03/src/main.R
@@ -0,0 +1,32 @@
+source("../../utils.R")
+session_cookie <- readLines("../../../../.aoc_session")
+input <- get_aoc(2024, 3, session_cookie)
+
+pattern <- "mul\\([0-9]*,[0-9]*\\)"
+p1 <- stringr::str_extract_all(input, pattern) |>
+  unlist() |>
+  stringr::str_replace_all("mul|\\(|\\)", "") |>
+  sapply(\(i) strsplit(i, ",") |> lapply(as.integer) |> unlist() |> prod()) |>
+  sum()
+print_result(2024, 3, p1)
+
+pattern <- "(don't\\(\\)|do\\(\\)|mul\\([0-9]*,[0-9]*\\))"
+instructions <- stringr::str_extract_all(input, pattern) |>
+  unlist() |>
+  stringr::str_replace_all("mul|\\(|\\)", "")
+
+p2 <- numeric(length=1)
+do <- TRUE
+
+for (i in seq_along(instructions)) {
+  if (instructions[[i]] == "don't") {
+    do <- FALSE
+    next
+  } else if (instructions[[i]] == "do") {
+    do <- TRUE
+    next
+  }
+  if (do)
+    p2 = p2 + strsplit(instructions[[i]], ",") |> lapply(as.integer) |> unlist() |> prod()
+}
+print_result(2024, 3, p2)
