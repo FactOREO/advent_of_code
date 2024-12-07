ad5ecb6f7d8146605a1693e9bd061f578cc75da0
FactOREO <hdustin@gmx.de>
2024-12-07 17:09:45 +0100

    Added minor adjustments; fixed typo

--- 2024/r/day07/src/main.R
+++ 2024/r/day07/src/main.R
@@ -1,11 +1,12 @@
 source("../../utils.R")
 session_cookie <- readLines("../../../../.aoc_session")
-input <- get_aoc(2024, 7, session_cookie)
-# input <- readLines("../input/test_input.txt")
+get_aoc(2024, 7, session_cookie)
+input <- readLines("../input/input.txt")
 
 #' Generate Operators
 #'
-#' Count of combinations: length(operators) ^ (length(numbers) - 1)
+#' Get all possible combinations for an arbitrary number of operators and combinations
+#' of those.
 #'
 #' @param len - How many places for the operators are requested
 #' @param operators - Character vectors of possible operators
@@ -15,15 +16,14 @@ generate_operators <- function(len, operators = c("*", "+", "||")) {
   n_com <- length(operators) ^ (len)
   len_operators <- length(operators)
   out <- matrix(NA_character_, ncol = len, nrow = n_com)
-  for (index in 1:n_com) {
-      for (i in 0:(len - 1)) {
-        op_index <- (index - 1) %/% len_operators^i %% len_operators
-        out[index, i + 1] <- operators[op_index + 1]
+  for (index in seq.int(1L, n_com)) {
+      for (i in seq.int(0L, (len - 1L))) {
+        op_index <- (index - 1L) %/% len_operators ^ i %% len_operators
+        out[index, i + 1L] <- operators[op_index + 1L]
       }
   }
   out
 }
-
 
 #' Calculate result
 #'
@@ -33,10 +33,10 @@ generate_operators <- function(len, operators = c("*", "+", "||")) {
 #' @param numbers The numbers to apply operation onto
 #' @param target_value The target value to reach as boundary
 #'
-#' @return int Result of the operations
+#' @return numeric Result of the operations or 0 if the value exceeds target.
 calculate_result <- function(operators, numbers, target_value) {
   multiplications <- length(which(operators == "*"))
-  concatenations <- length(which(operators == "||"))
+  concatenations  <- length(which(operators == "||"))
   if (multiplications == 0L && concatenations == 0L) return(sum(numbers))
   if (multiplications == length(operators)) return(prod(numbers))
   if (concatenations == length(operators)) return(as.numeric(paste(numbers, collapse="")))
@@ -59,11 +59,11 @@ p1 <- lapply(input, \(i) {
   values <- strsplit(i, ": ", fixed = TRUE)[[1L]]
   result <- values[[1L]] |> as.numeric()
   numbers <- strsplit(values[[2L]], " ", fixed = TRUE)[[1L]] |> as.numeric()
-  # cat(sprintf("Check if %s can be combined to %.0f\n", paste(numbers, collapse = ", "), result))
   operator_combinations <- generate_operators(length(numbers) - 1L, operators = c("*", "+"))
   results <- apply(operator_combinations, calculate_result, MARGIN = 1, numbers = numbers, target_value = result)
   if (any(results == result)) result
 }) |> unlist() |> sum()
+
 print_result(2024, 7, p1)
 # The result for day 7 of AOC 2024 is: 1260333054159
 
@@ -80,4 +80,6 @@ p2 <- mapply(\(i, idx) {
   if (idx %% 10 == 0) cat(sprintf("Finished iteration %d\n", idx))
   if (any(results == result)) result
 }, i = input, idx = seq_along(input)) |> unlist() |> sum()
+
 print_result(2024, 7, p2)
+# The result for day 7 of AOC 2024 is: 162042343638683
